1ef31ddc39ae35afeed3acba8f70cb772f2b4ef3
Collin Peters <cpeters@intouchfollowup.com>
2013-11-07 15:59:17 -0800

    Use generics. Bump version number

--- src/main/java/org/joda/time/mybatis/handlers/LocalDateTypeHandler.java
+++ src/main/java/org/joda/time/mybatis/handlers/LocalDateTypeHandler.java
@@ -14,81 +14,84 @@
 
 package org.joda.time.mybatis.handlers;
 
+import java.sql.CallableStatement;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedTypes;
 import org.apache.ibatis.type.TypeHandler;
 import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDate;
 
-import java.sql.*;
-
 @MappedTypes(LocalDate.class)
-public class LocalDateTypeHandler implements TypeHandler
+public class LocalDateTypeHandler implements TypeHandler<LocalDate>
 {
 
-    /* (non-Javadoc)
-     * @see org.apache.ibatis.type.TypeHandler#setParameter(java.sql.PreparedStatement, int, java.lang.Object, org.apache.ibatis.type.JdbcType)
-     */
-    public void setParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException
-    {
+	/* (non-Javadoc)
+	 * @see org.apache.ibatis.type.TypeHandler#setParameter(java.sql.PreparedStatement, int, java.lang.Object, org.apache.ibatis.type.JdbcType)
+	 */
+	public void setParameter(PreparedStatement ps, int i, LocalDate localDate, JdbcType jdbcType) throws SQLException
+	{
 
-        LocalDate date = (LocalDate) parameter;
-        if (date != null)
-        {
-            ps.setDate(i, new Date(date.toDateTimeAtStartOfDay().toDate().getTime()));
-        }
-        else
-        {
-            ps.setDate(i, null);
-        }
-    }
+		if (localDate != null)
+		{
+			ps.setDate(i, new Date(localDate.toDateTimeAtStartOfDay().toDate().getTime()));
+		}
+		else
+		{
+			ps.setDate(i, null);
+		}
+	}
 
-    /* (non-Javadoc)
-     * @see org.apache.ibatis.type.TypeHandler#getResult(java.sql.ResultSet, java.lang.String)
-     */
-    public Object getResult(ResultSet rs, String columnName) throws SQLException
-    {
-        Date date = rs.getDate(columnName);
-        if (date != null)
-        {
-            return new LocalDate(date.getTime(), DateTimeZone.UTC);
-        }
-        else
-        {
-            return null;
-        }
-    }
+	/* (non-Javadoc)
+	 * @see org.apache.ibatis.type.TypeHandler#getResult(java.sql.ResultSet, java.lang.String)
+	 */
+	public LocalDate getResult(ResultSet rs, String columnName) throws SQLException
+	{
+		Date date = rs.getDate(columnName);
+		if (date != null)
+		{
+			return new LocalDate(date.getTime(), DateTimeZone.UTC);
+		}
+		else
+		{
+			return null;
+		}
+	}
 
 	/* (non-Javadoc)
 	 * @see org.apache.ibatis.type.TypeHandler#getResult(java.sql.ResultSet, int)
 	 */
-	public Object getResult(ResultSet rs, int columnIndex) throws SQLException
+	public LocalDate getResult(ResultSet rs, int columnIndex) throws SQLException
 	{
-        Date date = rs.getDate(columnIndex);
-        if (date != null)
-        {
-            return new LocalDate(date.getTime(), DateTimeZone.UTC);
-        }
-        else
-        {
-            return null;
-        }
+		Date date = rs.getDate(columnIndex);
+		if (date != null)
+		{
+			return new LocalDate(date.getTime(), DateTimeZone.UTC);
+		}
+		else
+		{
+			return null;
+		}
 	}
 
-    /* (non-Javadoc)
-     * @see org.apache.ibatis.type.TypeHandler#getResult(java.sql.CallableStatement, int)
-     */
-    public Object getResult(CallableStatement cs, int columnIndex) throws SQLException
-    {
-        Date date = cs.getDate(columnIndex);
-        if (date != null)
-        {
-            return new LocalDate(date.getTime(), DateTimeZone.UTC);
-        }
-        else
-        {
-            return null;
-        }
-    }
+	/* (non-Javadoc)
+	 * @see org.apache.ibatis.type.TypeHandler#getResult(java.sql.CallableStatement, int)
+	 */
+	public LocalDate getResult(CallableStatement cs, int columnIndex) throws SQLException
+	{
+		Date date = cs.getDate(columnIndex);
+		if (date != null)
+		{
+			return new LocalDate(date.getTime(), DateTimeZone.UTC);
+		}
+		else
+		{
+			return null;
+		}
+	}
 
 }
