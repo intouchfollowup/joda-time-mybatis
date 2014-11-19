71e8f7eefaa22ec5347c763de543074a3e5f39f3
Collin Peters <cpeters@intouchfollowup.com>
2014-11-19 09:55:14 -0800

    Add LocalDateTimeTypeHandler

--- src/main/java/org/joda/time/mybatis/handlers/LocalDateTimeTypeHandler.java
+++ src/main/java/org/joda/time/mybatis/handlers/LocalDateTimeTypeHandler.java
@@ -1,44 +1,81 @@
-//Copyright 2012 Lucas Libraro
+// Copyright 2012 Lucas Libraro
 //
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//       http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-//package org.joda.time.mybatis.handlers;
-//
-//import java.sql.CallableStatement;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//
-//import org.apache.ibatis.type.JdbcType;
-//import org.apache.ibatis.type.TypeHandler;
-//import org.joda.time.DateTimeZone;
-//import org.joda.time.LocalDateTime;
-//
-//public class LocalDateTimeTypeHandler implements TypeHandler
-//{
-//    public void setParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException
-//    {
-//        ps.setTimestamp(i, new java.sql.Timestamp(((LocalDateTime) parameter).toDateTime(DateTimeZone.UTC).toDate()
-//                .getTime()));
-//    }
-//
-//    public Object getResult(ResultSet rs, String columnName) throws SQLException
-//    {
-//        return new LocalDateTime(rs.getTimestamp(columnName).getTime(), DateTimeZone.UTC);
-//    }
-//
-//    public Object getResult(CallableStatement cs, int columnIndex) throws SQLException
-//    {
-//        return new LocalDateTime(cs.getTimestamp(columnIndex).getTime(), DateTimeZone.UTC);
-//    }
-//}
+package org.joda.time.mybatis.handlers;
+
+import java.sql.CallableStatement;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedTypes;
+import org.apache.ibatis.type.TypeHandler;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDateTime;
+
+@MappedTypes(LocalDateTime.class)
+public class LocalDateTimeTypeHandler implements TypeHandler<LocalDateTime> {
+
+	/* (non-Javadoc)
+	 * @see org.apache.ibatis.type.TypeHandler#setParameter(java.sql.PreparedStatement, int, java.lang.Object, org.apache.ibatis.type.JdbcType)
+	 */
+	public void setParameter(PreparedStatement ps, int i, LocalDateTime localDateTime, JdbcType jdbcType)
+			throws SQLException {
+
+		if (localDateTime != null) {
+			ps.setDate(i, new Date(localDateTime.toDate().getTime()));
+		} else {
+			ps.setDate(i, null);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.ibatis.type.TypeHandler#getResult(java.sql.ResultSet, java.lang.String)
+	 */
+	public LocalDateTime getResult(ResultSet rs, String columnName) throws SQLException {
+		Date date = rs.getDate(columnName);
+		if (date != null) {
+			return new LocalDateTime(date.getTime(), DateTimeZone.UTC);
+		} else {
+			return null;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.ibatis.type.TypeHandler#getResult(java.sql.ResultSet, int)
+	 */
+	public LocalDateTime getResult(ResultSet rs, int columnIndex) throws SQLException {
+		Date date = rs.getDate(columnIndex);
+		if (date != null) {
+			return new LocalDateTime(date.getTime(), DateTimeZone.UTC);
+		} else {
+			return null;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.ibatis.type.TypeHandler#getResult(java.sql.CallableStatement, int)
+	 */
+	public LocalDateTime getResult(CallableStatement cs, int columnIndex) throws SQLException {
+		Date date = cs.getDate(columnIndex);
+		if (date != null) {
+			return new LocalDateTime(date.getTime(), DateTimeZone.UTC);
+		} else {
+			return null;
+		}
+	}
+
+}
